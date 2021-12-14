8868af3b736c1dffd8defbfd093020241ddefa05
Gadzhi Kharkharov <me@kkga.me>
2021-12-14 20:27:08 +0300

    wip: day 09 part 2

--- 09.go
+++ 09.go
@@ -3,9 +3,16 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
+
+type Point struct {
+	row int
+	col int
+	val int
+}
 
 func Day09() {
 	input, err := os.ReadFile("input/09.txt")
@@ -14,18 +21,88 @@ func Day09() {
 	lp := LowPoints(string(input))
 	var risk int
 
-	for _, p := range lp {
-		risk += 1 + p
+	for _, v := range lp {
+		risk += 1 + v
 	}
 
 	fmt.Println("Part 1:", risk)
+
+	basins := Basins(string(input))
+
+	sort.Slice(basins, func(i1, i2 int) bool {
+		return len(basins[i1]) > len(basins[i2])
+	})
+
+	mlt := 1
+	for _, b := range basins[:3] {
+		fmt.Println(len(b))
+		mlt *= len(b)
+	}
+
+	fmt.Println("Part 2:", mlt)
 }
 
-func LowPoints(input string) (points []int) {
+func LowPoints(input string) (points map[Point]int) {
+	points = make(map[Point]int)
+
+	hmap := heightmap(input)
+
+	for i, r := range hmap {
+		for j, n := range r {
+			p := Point{i, j, n}
+			if isLowPoint(hmap, p) {
+				points[p] = n
+			}
+		}
+	}
+
+	return
+}
+
+func Basins(input string) (basins [][]Point) {
+	lp := LowPoints(input)
+	hmap := heightmap(input)
+
+	var basin func(b []Point, step int, ns []Point) []Point
+
+	basin = func(b []Point, step int, ns []Point) []Point {
+		if step == 8 {
+			return b
+		}
+
+		var nns []Point
+		for _, n := range ns {
+			if n.val == step+1 {
+				b = append(b, n)
+				for _, nn := range neighbors(hmap, n) {
+					if !hasPoint(nns, nn) {
+						nns = append(nns, nn)
+					}
+				}
+			}
+		}
+		return basin(b, step+1, nns)
+	}
+
+	for p := range lp {
+		b := basin([]Point{p}, p.val, neighbors(hmap, p))
+		basins = append(basins, b)
+	}
+
+	return
+}
+
+func hasPoint(points []Point, p Point) bool {
+	for _, pp := range points {
+		if pp.col == p.col && pp.row == p.row {
+			return true
+		}
+	}
+	return false
+}
+
+func heightmap(input string) (hmap [][]int) {
 	rows := strings.Split(input, "\n")
-
-	var hmap [][]int
-
 	for _, r := range rows {
 		if r == "" {
 			continue
@@ -39,36 +116,28 @@ func LowPoints(input string) (points []int) {
 		}
 		hmap = append(hmap, row)
 	}
-
-	for i, r := range hmap {
-		for j, n := range r {
-			if isLowPoint(hmap, i, j) {
-				points = append(points, n)
-			}
-		}
-	}
-
 	return
 }
 
-func isLowPoint(hmap [][]int, row int, col int) bool {
-	var neighbors []int
+func neighbors(hmap [][]int, p Point) (neighbors []Point) {
+	if p.row > 0 {
+		neighbors = append(neighbors, Point{p.row - 1, p.col, hmap[p.row-1][p.col]})
+	}
+	if p.col > 0 {
+		neighbors = append(neighbors, Point{p.row, p.col - 1, hmap[p.row][p.col-1]})
+	}
+	if p.row < len(hmap)-1 {
+		neighbors = append(neighbors, Point{p.row + 1, p.col, hmap[p.row+1][p.col]})
+	}
+	if p.col < len(hmap[0])-1 {
+		neighbors = append(neighbors, Point{p.row, p.col + 1, hmap[p.row][p.col+1]})
+	}
+	return
+}
 
-	if row > 0 {
-		neighbors = append(neighbors, hmap[row-1][col])
-	}
-	if col > 0 {
-		neighbors = append(neighbors, hmap[row][col-1])
-	}
-	if row < len(hmap)-1 {
-		neighbors = append(neighbors, hmap[row+1][col])
-	}
-	if col < len(hmap[0])-1 {
-		neighbors = append(neighbors, hmap[row][col+1])
-	}
-
-	for _, num := range neighbors {
-		if num <= hmap[row][col] {
+func isLowPoint(hmap [][]int, p Point) bool {
+	for _, point := range neighbors(hmap, p) {
+		if point.val <= hmap[p.row][p.col] {
 			return false
 		}
 	}
--- 09_test.go
+++ 09_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"reflect"
+	"sort"
 	"testing"
 )
 
@@ -12,10 +14,39 @@ const testinput09 = `2199943210
 9899965678`
 
 func TestLowPoints(t *testing.T) {
-	want := []int{1, 0, 5, 5}
+	want := map[Point]int{
+		{0, 1, 1}: 1,
+		{0, 9, 0}: 0,
+		{2, 2, 5}: 5,
+		{4, 6, 5}: 5,
+	}
 	got := LowPoints(testinput09)
 
 	if !reflect.DeepEqual(want, got) {
 		t.Errorf("want %v, got %v", want, got)
+	}
+}
+
+func TestBasins(t *testing.T) {
+	want := 1134
+
+	basins := Basins(testinput09)
+
+	for _, b := range basins {
+		fmt.Println(b)
+	}
+
+	sort.Slice(basins, func(i1, i2 int) bool {
+		return len(basins[i1]) > len(basins[i2])
+	})
+
+	got := 1
+	for _, b := range basins[:3] {
+		// fmt.Println(len(b))
+		got *= len(b)
+	}
+
+	if want != got {
+		t.Errorf("want %d, got %d", want, got)
 	}
 }
