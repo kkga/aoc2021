f5e1b998ccc7c1229ba7cc4c77f4f61db84619cf
Gadzhi Kharkharov <me@kkga.me>
2021-12-15 16:53:57 +0300

    solve day 10 part 2

--- 10.go
+++ 10.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
+	"sort"
 	"strings"
 )
 
@@ -12,6 +14,13 @@ var scoreMap = map[rune]int{
 	']': 57,
 	'}': 1197,
 	'>': 25137,
+}
+
+var completionScoreMap = map[rune]int{
+	')': 1,
+	']': 2,
+	'}': 3,
+	'>': 4,
 }
 
 var charMap = map[rune]rune{
@@ -25,45 +34,74 @@ func Day10() {
 	input, err := os.ReadFile("input/10.txt")
 	ch(err)
 
-	score := CheckMatchingChars(string(input))
-	fmt.Println("Part 1:", score)
-}
+	lines := make(map[string][]rune)
+	corrupted := make(map[string]rune)
 
-func CheckMatchingChars(input string) int {
-	var score int
-
-	sc := bufio.NewScanner(strings.NewReader(input))
+	sc := bufio.NewScanner(strings.NewReader(string(input)))
 	sc.Split(bufio.ScanLines)
-
-	charStack := make([]rune, 0)
-	corrupted := make([]string, 0)
-	illegalChars := make([]rune, 0)
-
 	for sc.Scan() {
-	Out:
-		for _, ch := range sc.Text() {
-			switch ch {
-			case '(', '[', '{', '<':
-				charStack = append(charStack, ch)
-			default:
-				if ch != charMap[charStack[len(charStack)-1]] {
-					corrupted = append(corrupted, sc.Text())
-					illegalChars = append(illegalChars, ch)
-					break Out
-				} else {
-					charStack = charStack[:len(charStack)-1]
-				}
-			}
+		stack, err := IncompleteChars(sc.Text())
+		if err != nil {
+			corrupted[sc.Text()] = stack[0]
+		} else {
+			lines[sc.Text()] = stack
 		}
 	}
 
-	// for _, l := range corrupted {
-	// 	fmt.Println(l)
-	// }
+	p1score := 0
+	for _, v := range corrupted {
+		p1score += scoreMap[v]
+	}
+	fmt.Println("Part 1:", p1score)
 
-	for _, ch := range illegalChars {
-		score += scoreMap[ch]
+	p2scores := make([]int, 0)
+	for _, v := range lines {
+		score := 0
+		rev := reverse(v)
+		for _, ch := range rev {
+			score *= 5
+			score += completionScoreMap[charMap[ch]]
+		}
+		p2scores = append(p2scores, score)
+	}
+	sort.Ints(p2scores)
+
+	p2score := p2scores[len(p2scores)/2]
+	fmt.Println("Part 2:", p2score)
+}
+
+func IncompleteChars(line string) (stack []rune, err error) {
+	stack = make([]rune, 0)
+
+Out:
+	for _, ch := range line {
+		switch ch {
+		case '(', '[', '{', '<':
+			stack = append(stack, ch)
+		case ')', ']', '}', '>':
+			expected := charMap[stack[len(stack)-1]]
+			if ch != expected {
+				// fmt.Println(line)
+				// fmt.Printf("expected: %s, found: %s\n", string(expected), string(ch))
+				stack = []rune{ch}
+				err = errors.New("corrupted line")
+				break Out
+			} else {
+				stack = stack[:len(stack)-1]
+			}
+		}
+	}
+	return
+}
+
+func reverse(s []rune) []rune {
+	a := make([]rune, len(s))
+	copy(a, s)
+
+	for i := len(a)/2 - 1; i >= 0; i-- {
+		opp := len(a) - 1 - i
+		a[i], a[opp] = a[opp], a[i]
 	}
 
-	return score
+	return a
 }
--- 10_test.go
+++ 10_test.go
@@ -1,6 +1,11 @@
 package main
 
-import "testing"
+import (
+	"bufio"
+	"sort"
+	"strings"
+	"testing"
+)
 
 const testinput10 = `[({(<(())[]>[[{[]{<()<>>
 [(()[<>])]({[<{<<[]>>(
@@ -13,11 +18,49 @@ const testinput10 = `[({(<(())[]>[[{[]{<()<>>
 <{([([[(<>()){}]>(<<{{
 <{([{{}}[<[[[<>{}]]]>[]]`
 
-func TestCheckMatchingChars(t *testing.T) {
-	want := 26397
-	got := CheckMatchingChars(testinput10)
+func TestIncompleteChars(t *testing.T) {
+	want1 := 26397
+	want2 := 288957
 
-	if want != got {
-		t.Errorf("want %d, got %d", want, got)
+	sc := bufio.NewScanner(strings.NewReader(testinput10))
+	sc.Split(bufio.ScanLines)
+
+	lines := make(map[string][]rune)
+	corrupted := make(map[string]rune)
+
+	for sc.Scan() {
+		stack, err := IncompleteChars(sc.Text())
+		if err != nil {
+			corrupted[sc.Text()] = stack[0]
+		} else {
+			lines[sc.Text()] = stack
+		}
+	}
+
+	p1score := 0
+	for _, v := range corrupted {
+		p1score += scoreMap[v]
+	}
+
+	if p1score != want1 {
+		t.Fatalf("want %d, got %d", want1, p1score)
+	}
+
+	p2scores := make([]int, 0)
+	for _, v := range lines {
+		score := 0
+		rev := reverse(v)
+		for _, ch := range rev {
+			score *= 5
+			score += completionScoreMap[charMap[ch]]
+		}
+		p2scores = append(p2scores, score)
+	}
+	sort.Ints(p2scores)
+
+	p2score := p2scores[len(p2scores)/2]
+
+	if p2score != want2 {
+		t.Fatalf("want %d, got %d", want2, p2score)
 	}
 }
